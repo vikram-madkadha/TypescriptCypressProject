908d634261dbdd92b5f5514ea2180f5c3c6b4224
vikram-madkadha <vmadkadha@gmail.com>
2025-04-17 10:12:31 +0530

    chore: Added iframe plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+     
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
