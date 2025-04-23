aa447f405da127b518da0c684c6bfa850f4de774
vikram-madkadha <vmadkadha@gmail.com>
2025-04-23 22:07:48 +0530

    chore:added a full fledged test for save config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on); 
+      return config;
     },
   },
 });
